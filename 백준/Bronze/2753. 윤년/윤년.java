42ad67d754591c88a958458061d24b546c92436a
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2023-08-24 10:05:14 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner in = new Scanner(System.in);
+	    
+	    int year;
+	    
+	    year = in.nextInt();
+	    
+	    if ((year >= 1) && (year <= 4000)) {
+	        if ((year%4==0) && ( (year%100!=0) || (year%400==0) )) {
+	            System.out.println("1");
+	        } else
+	            System.out.println("0");
+	    
+	    }
+	    
+	}
+}
