670117bc68f74a3e8728b6dadf528f642f6364e7
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2023-08-24 10:03:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    long a, b, c;
+	    
+	    Scanner in = new Scanner (System.in);
+	    
+	    a = in.nextLong();
+	    b = in.nextLong(); 
+	    c = in.nextLong(); 
+        
+		System.out.printf("%d", a+b+c);
+
+	}
+}
