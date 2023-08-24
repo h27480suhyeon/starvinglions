5cefd7c845059c2dcbd39f5e4a63b75fe41b4af2
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2023-08-24 09:55:49 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+
+public class Main {
+	public static void main(String[] args) {
+	    
+	    Scanner in = new Scanner(System.in);
+	    
+	    int a, b;
+	    a = in.nextInt();
+	    b = in.nextInt();
+	    
+		System.out.printf("%d", a+b);
+	}
+}
