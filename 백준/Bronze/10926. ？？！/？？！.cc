01c887923db47255e454b3311cd00266fd4be239
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2023-12-28 17:38:41 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    string b = "??!";
+    
+    cin >> a;
+    cout << a <<b << endl;
+    return 0;
+}
