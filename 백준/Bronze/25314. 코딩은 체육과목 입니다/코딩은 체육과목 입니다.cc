30f00701be1527e92d815165cc287057fe1c650a
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2024-01-04 10:55:46 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str = "long";
+    int n;
+    
+    cin >> n;
+    
+    for(int i=0; i<n/4; i++) {
+        cout << str << " ";
+    } cout << "int";
+    
+    
+    return 0;
+}
