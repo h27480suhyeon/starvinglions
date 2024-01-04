533737ecbe7a716cc7c7b2f6924fd93db1aafba4
h27480suhyeon <125898993+h27480suhyeon@users.noreply.github.com>
2024-01-04 09:35:22 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    int a,b;
+    
+    cin >> t;
+    
+    for(int i=1; i<=t; i++) {
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    
+    
+    
+    return 0;
+}
